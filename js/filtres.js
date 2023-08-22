64bf130d42b61e37a0d4473471e4e7371db271a5
Jean-Charles <108011439+JCharles28@users.noreply.github.com>
2023-08-22 03:14:24 +0200

    Update : test 1 videos

--- js/filtres.js
+++ js/filtres.js
@@ -2,57 +2,57 @@
 
 $("#AaZ").click(function () {
   let tabItems = Array.from($(".item"));
-  $tab = $(tabItems);
+  let tab = $(tabItems);
 
-  $tab.sort((a, b) => {
+  tab.sort((a, b) => {
     let titre1 = $(a).find(".title").text();
     let titre2 = $(b).find(".title").text();
 
     return titre1.localeCompare(titre2);
   });
 
-  $(".video-container").append($tab);
-  $tab.detach();
-  $(".video-container").append($tab);
+  $(".video-container").append(tab);
+  tab.detach();
+  $(".video-container").append(tab);
 });
 
 $("#ZaA").click(function () {
   let tabItems = Array.from($(".item"));
-  $tab = $(tabItems);
+  let tab = $(tabItems);
 
-  $tab.sort((a, b) => {
+  tab.sort((a, b) => {
     let titre1 = $(a).find(".title").text();
     let titre2 = $(b).find(".title").text();
 
     return titre2.localeCompare(titre1);
   });
 
-  $(".video-container").append($tab);
-  $tab.detach();
-  $(".video-container").append($tab);
+  $(".video-container").append(tab);
+  tab.detach();
+  $(".video-container").append(tab);
 });
 
 $("#femmes").click(function () {
   let tabItems = Array.from($(".item"));
-  $tab = $(tabItems);
+  let tab = $(tabItems);
 
   let tabItemsF = Array.from($(".f"));
-  $tabF = $(tabItemsF);
+  tabF = $(tabItemsF);
 
-  $(".video-container").append($tab);
-  $tab.detach();
-  $(".video-container").append($tabF);
+  $(".video-container").append(tab);
+  tab.detach();
+  $(".video-container").append(tabF);
 });
 
 function hommes_tri() {
     let tabItems = Array.from($(".item"));
-    $tab = $(tabItems);
+    let tab = $(tabItems);
   
     let tabItemsH = Array.from($(".h"));
-    $tabH = $(tabItemsH);
+    tabH = $(tabItemsH);
 
-    $(".video-container").append($tab);
-    $tab.detach();
-    $(".video-container").append($tabH);
+    $(".video-container").append(tab);
+    tab.detach();
+    $(".video-container").append(tabH);
   }
 $("#hommes").click(hommes_tri);
